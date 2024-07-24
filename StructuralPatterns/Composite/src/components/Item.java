d08890b1d6b89163e4bbc0fc4e6909f03662cce2
Jeferson Lors <lors.jeferson@gmail.com>
2024-07-24 14:58:14 -0300

    Composite's implementation finished.

---
+++ StructuralPatterns/Composite/src/components/Item.java
@@ -0,0 +1,12 @@
+package components;
+
+public interface Item {
+    double getVolume();
+    double getFreeSpace();
+    double getUsedSpace();
+    double getWeight();
+    void seeInside();
+    Item addItem(Item item) throws Exception;
+    Item removeItem(Item item) throws Exception;;
+    Item getItem(int itemCode) throws Exception;
+}
